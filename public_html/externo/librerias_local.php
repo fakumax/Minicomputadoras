3a788435f2cbcb3cb7bc99d5e0aac30f5a356969
marceloleandrofernandez <marceloleandrofernandez@gmail.com>
2018-04-24 23:48:00 -0300

    Add files via upload

---
+++ public_html/externo/librerias_local.php
@@ -0,0 +1,71 @@
+
+<!-- Librerías -->
+
+<!-- Bootstrap 3 -->
+<link rel="stylesheet" href="externo/bootstrap3/css/bootstrap.min.css">
+<script src="externo/jquery/jquery.min.js"></script>
+<script src="externo/bootstrap3/js/bootstrap.min.js"></script>
+
+<!-- DataTable para Bootstrap 3 -->
+<link rel="stylesheet" href="externo/datatables/css/dataTables.bootstrap.min.css">
+<script src="externo/datatables/js/jquery.dataTables.es.js"></script>
+<script src="externo/datatables/js/dataTables.bootstrap.min.js"></script>
+
+<!-- Exportar Datatables -->
+<link rel="stylesheet" href="externo/exportar/buttons.dataTables.min.css">
+<script src="externo/exportar/dataTables.buttons.min.js"></script>
+<script src="externo/exportar/jszip.min.js"></script>
+<script src="externo/exportar/pdfmake.min.js"></script>
+<script src="externo/exportar/vfs_fonts.js"></script>
+<script src="externo/exportar/buttons.html5.min.js"></script>
+<script src="externo/exportar/buttons.colVis.min.js"></script>
+
+<!-- Select2 para Bootstrap -->
+<link rel="stylesheet" href="externo/select2/css/select2.min.css">
+<link rel="stylesheet" href="externo/select2/css/select2-bootstrap.css">
+<script src="externo/select2/js/select2.js"></script>
+
+<!-- Autocomplete -->
+<link rel="stylesheet" href="externo/autocomplete/css/jquery-ui.css">
+<script src="externo/autocomplete/js/jquery-ui.js"></script>
+
+<!-- Alertify -->
+<link rel="stylesheet" href="externo/alertify/css/alertify.min.css">
+<link rel="stylesheet" href="externo/alertify/css/themes/bootstrap.min.css">
+<script src="externo/alertify/js/alertify.min.js"></script>
+
+<!-- DateTimePicker -->
+<link rel="stylesheet" href="externo/datetimepicker/css/bootstrap-datetimepicker.min.css">
+<script src="externo/datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+<script src="externo/datetimepicker/js/bootstrap-datetimepicker.es.js"></script>
+
+<!-- Chart JS -->
+<script src="externo/chart/Chart.min.js"></script>
+
+<!-- Morris -->
+<link rel="stylesheet" href="externo/prettify/prettify.min.css">
+<link rel="stylesheet" href="externo/morris/morris.css">
+<script src="externo/raphael/raphael-min.js"></script>
+<script src="externo/morris/morris.js"></script>
+<script src="externo/prettify/prettify.min.js"></script>
+
+<!-- Ckeditor basic-standard-full -->
+<script src="externo/ckeditor/full/ckeditor.js"></script>
+
+<!-- Estilo Personalizado -->
+<link rel="stylesheet" href="externo/css/estilos.css">
+<link rel="stylesheet" href="externo/css/navbar.css">
+
+<!-- info-box , colores y tamaños de fuentes -->
+<link rel="stylesheet" href="externo/css/nuevo.css">
+
+<!-- countTo -->
+<script src="externo/jquery-countTo/jquery.countTo.js"></script>
+
+<!-- countTo -->
+<script src="externo/bootstrap-filestyle/bootstrap-filestyle.min.js"></script>
+
+<!-- material-design-iconic-font -->
+<!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  rel="stylesheet"> -->
+
---
+++ public_html/externo/librerias_web.php
@@ -0,0 +1,70 @@
+
+<!--seccion_head.php -->
+
+<!--
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="author" content="Autor">
+<meta name="description" content="Descripcion">
+<meta name="keywords" content="Palabras, Clave">
+
+<title>Titulo</title>
+
+-->
+<!--<link rel="icon" href="img/icono.ico">-->
+
+<!--seccion_css.php -->
+
+<!--//Bootstrap 3-->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!--//Estilo Personalizado-->
+<link rel="stylesheet" href="//yosukopizzas.000webhostapp.com/css/estilos.css">
+
+<!--//Datatables Bootstrap 3-->
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+
+<!--//Exportar Datatables-->
+<link rel="stylesheet" href="//cdn.datatables.net/buttons/1.4.1/css/buttons.dataTables.min.css">
+
+<!--//Select2 para Bootstrap-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-css/1.4.6/select2-bootstrap.css">
+
+<!--//Autocomplete-->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+
+<!-- //Alertify -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.0/build/css/alertify.min.css"/>
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.0/build/css/themes/bootstrap.min.css"/>
+
+<!--seccion_js.php -->
+
+<!--//Bootstrap 3-->
+<!-- jQuery first, then Bootstrap JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<!--//DataTable Bootstrap 3-->
+<script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+
+<!--//Exportar Datatables-->
+<script src="//cdn.datatables.net/buttons/1.4.1/js/dataTables.buttons.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script src="//cdn.datatables.net/buttons/1.4.1/js/buttons.html5.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.0.3/js/buttons.colVis.min.js"></script>
+
+<!--//Select2 para Bootstrap 3-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+<!--//Autocomplete-->
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<!-- //Alertify -->
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.0/build/alertify.min.js"></script>
